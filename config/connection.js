690ab63559d41a4478799dfba51c0a84a1d1b4de
damonk@gmail.com <damonkd@gmail.com>
2019-06-05 23:03:28 -0700

    added jawsdb for heroku

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,16 @@ var mysql = require('mysql');
 
 var connection;
 
-
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
     connection = mysql.createConnection({
         host: 'localhost',
         user: 'root',
         password: 'password',
         database: 'burgers_db'
     });
+}
 
 
 connection.connect(function (err) {
